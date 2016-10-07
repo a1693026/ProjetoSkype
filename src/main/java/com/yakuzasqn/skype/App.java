e44d8087124584c58c27b194da73d71ac64b9fdc
Glauco <shadowy7777@gmail.com>
2016-10-07 19:36:27 -0300

    Primeiro commit

---
+++ src/main/java/com/yakuzasqn/skype/App.java
@@ -0,0 +1,13 @@
+package com.yakuzasqn.skype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
